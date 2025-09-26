20d1446eb306176d9a33294c0a5dabc107bace85
josewolffsantiago <jose.gustavo@estudante.ifmt.edu.br>
2025-09-26 20:58:36 +0000

    Update main.py on e68512a77a85774b4f630ba0844be6140030e356 [skip ci]

--- hello-app/main/main.py
+++ hello-app/main/main.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 async def root():
-    return {"message": "Hello World v3"}
+    return {"message": "Hello World from docker hello-app:e68512a77a85774b4f630ba0844be6140030e356"}
